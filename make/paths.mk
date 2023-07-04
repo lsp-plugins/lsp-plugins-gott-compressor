eb1656758ad01011dfd1b268668f6475739a892d
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-07-05 01:14:57 +0300

    Updated build scripts and dependencies

--- make/paths.mk
+++ make/paths.mk
@@ -1,21 +1,21 @@
 #
-# Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
-#           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+# Copyright (C) 2023 Linux Studio Plugins Project <https://lsp-plug.in/>
+#           (C) 2023 Vladimir Sadovnikov <sadko4u@gmail.com>
 #
-# This file is part of lsp-plugins-plugin-template
+# This file is part of lsp-plugins-gott-compressor
 #
-# lsp-plugins-plugin-template is free software: you can redistribute it and/or modify
+# lsp-plugins-gott-compressor is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # any later version.
 #
-# lsp-plugins-plugin-template is distributed in the hope that it will be useful,
+# lsp-plugins-gott-compressor is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with lsp-plugins-plugin-template.  If not, see <https://www.gnu.org/licenses/>.
+# along with lsp-plugins-gott-compressor.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 # Installation prefix
