0dc4982eb78fafa3210f98b520be85b60535ef17
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-07-14 22:15:39 +0300

    Fixed improper behaviour of the bypass switch.

--- src/main/plug/gott_compressor.cpp
+++ src/main/plug/gott_compressor.cpp
@@ -1181,7 +1181,7 @@ namespace lsp
                     c->pOutLvl->set_value(level);
 
                     // Apply bypass
-                    c->sBypass.process(c->vOut, c->vInBuffer, c->vBuffer, to_process);
+                    c->sBypass.process(c->vOut, c->vIn, c->vBuffer, to_process);
 
                     // Update pointers
                     c->vIn             += to_process;
