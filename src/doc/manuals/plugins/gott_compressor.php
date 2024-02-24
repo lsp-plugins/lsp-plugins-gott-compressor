55d7233e0c77a616fe16cc07e773691e81e5f3ac
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-02-24 21:53:43 +0300

    Updated documentation

--- src/doc/manuals/plugins/gott_compressor.php
+++ src/doc/manuals/plugins/gott_compressor.php
@@ -95,6 +95,7 @@
 	<li><b>Output</b> - the amount of gain applied to the output signal before processing.</li>
 	<li><b>Dry</b> - the amount of dry (unprocessed) signal passed to the output.</li>
 	<li><b>Wet</b> - the amount of wet (processed) signal passed to the output.</li>
+	<li><b>Dry/Wet</b> - the knob that controls the balance between the mixed dry and wet signal (see <b>Dry</b> and <b>Wet</b>) and the dry (unprocessed) signal.</li>
 	<li><b>In</b> - the input signal meter.</li>
 	<li><b>Out</b> - the output signal meter.</li>
 </ul>
